aaef44b1bae58d2b695f5206c4d32201e93d1763
xulbux <xulbux.real@gmail.com>
2026-08-10 08:09:31 +0200

    Update configs

--- settings/shell/Microsoft.PowerShell_profile.ps1
+++ settings/shell/Microsoft.PowerShell_profile.ps1
@@ -5,64 +5,76 @@
 
 ######################################## STARSHIP ########################################
 
-# INITIALIZE STARSHIP
-Invoke-Expression (&starship init powershell)
-Enable-TransientPrompt
+try {
 
-# DISPLAY SIMPLIFIED PROMPT FUNCTION
-function Invoke-Starship-TransientFunction {
-    # CLEAR DETAILED PROMPT
-    [Console]::SetCursorPosition(0, [Math]::Max(0, $host.UI.RawUI.CursorPosition.Y))
-    [Console]::Write("`e[J")
-    # RETURN SIMPLIFIED PROMPT
-    $output = &starship module character
-    $stripped = $output -replace "`e\[[0-9;]*m", ""
-    "`e[1;35m$stripped`e[0m"
-}
+    # Initialize starship:
+    Invoke-Expression (&starship init powershell)
+    Enable-TransientPrompt
+
+    # Display simplified prompt function:
+    function Invoke-Starship-TransientFunction {
+        # Clear detailed prompt:
+        [Console]::SetCursorPosition(0, [Math]::Max(0, $host.UI.RawUI.CursorPosition.Y))
+        [Console]::Write("`e[J")
+        # Return simplified prompt:
+        $output = &starship module character
+        $stripped = $output -replace "`e\[[0-9;]*m", ""
+        "`e[1;35m$stripped`e[0m"
+    }
+
+} catch {}
 
 
 ###################################### INTELLISENSE ######################################
 # Install-Module -Name PSReadLine -Force
 
-# ENABLE PREDICTION BASED ON COMMAND HISTORY
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle InlineView
+try {
 
-# KEYBINDINGS FOR TAB COMPLETION
-Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+    # enable prediction based on command history:
+    Set-PSReadLineOption -PredictionSource History
+    Set-PSReadLineOption -PredictionViewStyle InlineView
 
-# ACCEPT NEXT WORD OF INLINE SUGGESTION
-Set-PSReadLineKeyHandler -Key Shift+RightArrow -Function AcceptNextSuggestionWord
+    # Keybindings for tab completion:
+    Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
-# PASTE AS SINGLE UNDO UNIT
-Set-PSReadLineKeyHandler -Key Ctrl+V -Function Paste
+    # Accept next word of inline suggestion:
+    Set-PSReadLineKeyHandler -Key Shift+RightArrow -Function AcceptNextSuggestionWord
+
+    # Paste as single undo unit:
+    Set-PSReadLineKeyHandler -Key Ctrl+V -Function Paste
+
+} catch {}
 
 
 ######################################## HISTORY #########################################
 
-# KEYBINDINGS FOR HISTORY SEARCH
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+try {
 
-# MOVE CURSOR TO END OF LINE WHEN RECALLING HISTORY ON EMPTY LINE
-Set-PSReadLineKeyHandler -Key UpArrow -ScriptBlock {
-    [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchBackward()
-    [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
-}
-Set-PSReadLineKeyHandler -Key DownArrow -ScriptBlock {
-    [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchForward()
-    [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
-}
+    # Keybindings for history search:
+    Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+    Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-# COMMAND HISTORY BROWSER
-Set-PSReadLineKeyHandler -Chord "Ctrl+h" -ScriptBlock {
-    $commands = Get-Content (Get-PSReadLineOption).HistorySavePath | Select-Object -Unique
-    [array]::Reverse($commands)
-
-    $selectedCommand = $commands | Out-GridView -Title "Command History" -PassThru
-
-    if ($selectedCommand) {
-        [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-        [Microsoft.PowerShell.PSConsoleReadLine]::Insert($selectedCommand)
+    # Move cursor to end of line when recalling history on empty line:
+    Set-PSReadLineKeyHandler -Key UpArrow -ScriptBlock {
+        [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchBackward()
+        [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
     }
-}
+    Set-PSReadLineKeyHandler -Key DownArrow -ScriptBlock {
+        [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchForward()
+        [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
+    }
+
+    # Command history browser:
+    Set-PSReadLineKeyHandler -Chord "Ctrl+h" -ScriptBlock {
+        $commands = Get-Content (Get-PSReadLineOption).HistorySavePath | Select-Object -Unique
+        [array]::Reverse($commands)
+
+        $selectedCommand = $commands | Out-GridView -Title "Command History" -PassThru
+
+        if ($selectedCommand) {
+            [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+            [Microsoft.PowerShell.PSConsoleReadLine]::Insert($selectedCommand)
+        }
+    }
+
+} catch {}
